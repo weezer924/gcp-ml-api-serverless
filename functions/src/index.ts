83b558f64ee1e46208011a072ea270bff00cef51
jakushin.cho <jakushin.cho@shift-plus.jp>
2018-10-18 12:55:53 +0900

    音声認識APIのログを追加した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,13 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as speech from '@google-cloud/speech'
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
 export const speechToTextFunc = functions.https.onRequest((request, response) => {
   const param: string = request.query.fileName;
   if(!param) {
     console.error('ERROR: fileName is null.');
-    response.end();
+    response.send(400).end();
   }
 
   // 音声ファイルの設定
@@ -28,14 +28,16 @@ export const speechToTextFunc = functions.https.onRequest((request, response) =>
   const client = new speech.SpeechClient();
   client.longRunningRecognize(payload).then(data => {
     const res = data[0];
+    console.log('Res is promise');
     return res.promise();
   }).then(data => {
     const res = data[0];
     const transcription = res.results.map(result => result.alternatives[0].transcript).join('\n');
+    console.log('Trascription finished! Try to write DB');
     const ref = admin.firestore().collection('orgText').doc(param).set({name: param, text: transcription})
     .then(() => {
-      console.error('Document successfully written!');
-      response.end();
+      console.log('Document successfully written!');
+      response.send(200).end();
     });
   }).catch(err => {
     console.error('ERROR:', err);
